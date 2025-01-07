4a71b59c9cb6f7354cc6dcf3f2176dcc3cfca37d
yang cao <2233684404@qq.com>
2025-01-07 11:05:52 +0100

    code

---
+++ main.py
@@ -0,0 +1,58 @@
+import typing
+import aas_middleware
+import json
+class BillOfMaterialInfo(aas_middleware.SubmodelElementCollection):
+    manufacterer: str
+    product_type: str
+
+class BillOfMaterial(aas_middleware.Submodel):
+    components: typing.List[str]
+    bill_of_material_info: BillOfMaterialInfo
+
+
+class ProcessModel(aas_middleware.Submodel):
+    processes: typing.List[str]
+
+
+class Product(aas_middleware.AAS):
+    bill_of_material: BillOfMaterial
+    process_model: typing.Optional[ProcessModel]
+
+example_product = Product(
+    id="example_product_id",
+    id_short="example_product_id",
+    description="Example Product",
+    bill_of_material=BillOfMaterial(
+        id="example_bom_id",
+        id_short="example_bom_id",
+        description="Example Bill of Material",
+        components=["component_1", "component_2"],
+        bill_of_material_info=BillOfMaterialInfo(
+            id="example_bom_info_id",
+            id_short="example_bom_info_id",
+            description="Example Bill of Material Info",
+            manufacterer="Example Manufacterer",
+            product_type="Example Product Type",
+        ),
+    ),
+    process_model=ProcessModel(
+        id="example_process_model_id",
+        id_short="example_process_model_id",
+        description="Example Process Model",
+        processes=["process_1", "process_2"],
+    ),
+)
+
+data_model = aas_middleware.DataModel.from_models(example_product)
+basyx_object_store = aas_middleware.formatting.BasyxFormatter().serialize(data_model)
+json_aas = aas_middleware.formatting.AasJsonFormatter().serialize(data_model)
+formatted_json = json.dumps(json_aas, indent=4, ensure_ascii=False)
+print(formatted_json)
+
+middleware = aas_middleware.Middleware()
+middleware.load_data_model("example", data_model, persist_instances=True)
+middleware.generate_rest_api_for_data_model("example")
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(middleware.app)
